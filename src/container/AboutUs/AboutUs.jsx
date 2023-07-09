2292970c2aaaebbc49f34c241ada3e331bf1071f
Galina Ivanova <52123474+GalyaIT@users.noreply.github.com>
2023-07-09 23:39:09 +0300

    Created Countdown component

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -14,7 +14,7 @@ const AboutUs = () => (
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img"/>
         <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
-        <Link to="*"><button type="button" className="custom__button">Know More</button></Link>        
+        <Link to="/coming-soon"><button type="button" className="custom__button">Know More</button></Link>        
       </div>
 
       <div className="app__aboutus-content_knife flex__center">
@@ -24,7 +24,7 @@ const AboutUs = () => (
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img"/>
         <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
-         <Link to="*"> <button type="button" className="custom__button">Know More</button></Link>      
+         <Link to="/coming-soon"> <button type="button" className="custom__button">Know More</button></Link>      
          </div>
 
     </div>
